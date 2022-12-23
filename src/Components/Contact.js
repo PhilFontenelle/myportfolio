3d319e3eef99305a7bbc322d6e5cd7db9dd342b9
philbert <n01192769@outlook.com>
2022-12-22 21:33:19 -0500

    modified

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-const linked = "www.linkedin.com/in/philbert-fontenelle/";
+const git = "https://github.com/PhilFontenelle?tab=repositories"
+const linked = "https://www.linkedin.com/in/philbert-fontenelle"
+
 let mEmail = "ordell_p@hotmail.com";
 let number = 4165090163;
 
@@ -8,16 +10,23 @@ let number = 4165090163;
 
 function Contact(){
 
-	const contact =  <h1> Contact info   </h1>
-	
+	const contact = (
+		<>
+		<h1> Contact info   </h1>
+		<b><i>{mEmail}</i> </b>
+		</>
+	)
 
-	const git = "https://github.com/PhilFontenelle?tab=repositories"
 
 	return(
 	<div> 
 		<h1>{contact}</h1>
 	<h3>
 		< a href={git} target="_blank">GitHub</a>
+	</h3>
+	
+	<h3>
+		<a href={linked} target="_blank">Linkedin</a>
 	</h3>
 	</div>
 	)

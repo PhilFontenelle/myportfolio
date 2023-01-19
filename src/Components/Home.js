a339f0c4a41af9181ffd96289e93f8497029d7f8
philbert <n01192769@outlook.com>
2023-01-18 23:37:47 -0500

    updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {motion} from "framer-motion"
+
+function RandomQuotes(quotes){
+    return quotes[Math.floor(Math.random()*quotes.length)];
+    }
+
+export default function Home () {
+   
+
+    const [quotes,SetQuotes]=useState([]);
+    const [quote,SetQuote]=useState(null);  
+    
+    useEffect(()=>{
+        ShowQuote()
+    },[quotes])
+    
+
+    
+ 
+    useEffect(()=>{
+
+        fetch("../quotes.json")
+        .then((res)=>res.json())
+        .then((json)=>{
+            SetQuotes(json);
+            SetQuote(json[1]);
+        });
+     },[])
+
+     function ShowQuote(){
+
+        SetQuote(RandomQuotes(quotes))
+    }
 
 
-const Home = () => (
+return(   
+    
 <div>
-
+        
 
  <h1>My Portfolio </h1>
  <h2>Philbert Fontenelle </h2>    
+<motion.div
+//animate={{x:0, y:0, scale:1}}
+drag //="x||y"
+>
+<section>
+<button onClick={ShowQuote}>Random</button>
+<h3 onClick={ShowQuote}>
+    
+    "{quote?.text}"
+</h3>
+<i>-{quote?.author}</i>
+</section>
+</motion.div>
 
 
 
@@ -14,10 +61,6 @@ const Home = () => (
 
 
 
+</div>)
+}
 
-
-
-</div>
-);
-
-export default Home;
